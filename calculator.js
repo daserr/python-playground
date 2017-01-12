5207f54c793d836e91d4c97066d14f2403dd5f32
DANIELA <daniela.serrano.p@gmail.com>
2017-01-11 19:34:04 -0500

    multiply 5 and 6

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function add(String) {
+    return a + b 
+}
+function subtract(String) {
+    return a - b 
+}
+function multiply(String) {
+    return a * b 
+}
+function divide(String) {
+    return a / b 
+}
